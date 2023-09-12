9a107aa009094e3ae0c71358c0396349e74aef07
thiago linch <dev.thiagolinch@gmail.com>
2023-09-12 08:39:49 -0400

    front end, install dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,141 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Github, FileVideo, Upload, Wand2 } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Textarea } from "@/components/ui/textarea"
+import { Separator } from "@/components/ui/separator"
+import { Label } from "@/components/ui/label"
+import {   
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select"
+import { Slider } from "@/components/ui/slider"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+export function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=" min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+        
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvolvido com 💜</span>
+          <Button variant={"outline"}>
+            <Github className="mr-2 h-4 w-4"/>
+            GitHub
+          </Button>
+        </div>
+
+
       </div>
-      <h1>upload.ai</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+  <main className="flex-1 p-6 flex gap-6">
+    <div className=" flex flex-col flex-1 gap-4">
+      <div className="grid grid-rows-2 gap-4 flex-1">
+        <Textarea
+        className="resize-none p-4 leading-relaxed" 
+          placeholder="Inclua o propt para a IA..." 
+        />
+        <Textarea
+        className="resize-none p-4 leading-relaxed" 
+          placeholder="Resultado gerado pela IA..."
+          readOnly
+
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+      <p className="text-sm text-muted-foreground">
+        Lembre-se você pode utilizar o <code className="text-violet-400 ">{'{trasncription}'}</code> no seu prompt para adicionar o conteúdo da transcição do vídeo selecionado.
       </p>
-    </>
+    </div>
+
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rouded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo w-4 h-4 />
+              Selcione um vídeo
+            </label>
+
+            <input type="file" id="video" accept="video/mp4" className="sr-only"/>
+
+            <Separator />
+
+            <div>
+              <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+              <Textarea
+                id="transcription_prompt"
+                className="h-20 leading-relaxed resize-none"
+                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Carregar video
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+
+          <Separator />
+
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Título do YouTube</SelectItem>
+                  <SelectItem value="description">Descrição do YouTube</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-sm text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                defaultValue={[0.5]}
+              />
+              <span className="block text-sm text-muted-foreground italic leading-relaxed">
+                Valores mais altor tendem a deixar o resultado mais criativo e com possíveis erros.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </aside>
+      </main>
+    </div>
   )
 }
-
-export default App
