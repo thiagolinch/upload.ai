4ab37b4439c3c34ee43c1419fec36682d93cd358
Thiago Linchin <79920937+devthiagolinch@users.noreply.github.com>
2023-10-03 11:12:16 -0400

    Update axios.ts (vercel)

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "https://upload-ai-server-sepia.vercel.app/"
+    baseURL: "https://upload-ai-server-0cf7.onrender.com/"
 })
 
 export default api
